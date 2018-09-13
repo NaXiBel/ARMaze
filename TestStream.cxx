ab2caabb1a689ce60e9e0366c9df6d0738cefc73
Alex <none>
2018-09-13 17:43:53 +0200

    Début projet : Récupération d'un flux vidéo d'une webcam intégrée.

---
+++ TestStream.cxx
@@ -0,0 +1,49 @@
+// g++ -I"C:\Program Files\opencv\build\include" -L"C:\Program Files\opencv\release\lib" -g -o Test.exe TestStream.cxx 
+// -lopencv_core343 -lopencv_imgproc343 -lopencv_highgui343 -lopencv_imgcodecs343 -lopencv_videoio343
+
+#include "opencv2/opencv.hpp"
+#include "opencv2/videoio.hpp"
+#include "opencv2/imgproc.hpp"
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+
+using namespace std;
+using namespace cv;
+
+/* Global variables */
+String window_name = "Capture";
+
+/* @function main */
+int main(void) {
+    VideoCapture capture;
+    Mat frame;
+    
+    namedWindow(window_name ,WINDOW_AUTOSIZE);
+    
+    // Read the video stream
+    capture.open(0);
+    if(!capture.isOpened()) { 
+        printf("--(!)Error opening video capture\n"); 
+        return -1; 
+    }
+    
+    capture.set(CV_CAP_PROP_FRAME_WIDTH , 640); 
+    capture.set(CV_CAP_PROP_FRAME_HEIGHT , 480); 
+    capture.set (CV_CAP_PROP_FOURCC, CV_FOURCC('B', 'G', 'R', '3'));
+
+    while(capture.read(frame)) {
+        if(frame.empty()) {
+            printf("(!) No captured frame -- Break!");
+            break;
+        }
+
+        imshow(window_name, frame);
+        int c = waitKey(10);
+        if((char)c == 'q') { 
+            break; 
+        }
+    }
+
+    return 0;
+}
