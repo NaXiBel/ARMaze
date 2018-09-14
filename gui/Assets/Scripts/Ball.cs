5d172c2a50b7ba5b5a3ea7fe84fab588c3a8c85b
Amélie <amelielupo@gmail.com>
2018-09-14 14:44:50 +0200

    initial commit

---
+++ gui/Assets/Scripts/Ball.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+
+	void Start () {
+		
+	}
+	
+	void Move() {
+
+	}
+	
+	void Update () {
+		
+	}
+}
