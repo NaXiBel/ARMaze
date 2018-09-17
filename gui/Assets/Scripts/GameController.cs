d8db534b64f1548c5399829a95d76af77fc8a801
Amélie <amelielupo@gmail.com>
2018-09-17 15:10:47 +0200

    Commit IHM base + textures & obstacles

--- gui/Assets/Scripts/GameController.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class GameController : MonoBehaviour {
-    public void CheckoutScene(int sceneId) {
-        SceneManager.LoadScene(sceneId);
-    }
-}
