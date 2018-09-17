95917fa9dfbc4b63dd3fd761e071af96a906e30e
Alex <none>
2018-09-17 12:38:28 +0200

    Ecran Game Over.

--- gui/Assets/Scripts/Chrono.cs
+++ gui/Assets/Scripts/Chrono.cs
@@ -3,36 +3,13 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Chrono : MonoBehaviour {
-    bool stopped;
-    float startTime;
-    float ellapsedTime;
+    float time;
 
     public Text chrono;
 
-    public void StartChrono() {
-        this.stopped = false;
-    }
-
-    public void StopChrono() {
-        this.stopped = true;
-    }
-
-    public void ResetChrono() {
-        this.ellapsedTime = 0;
-        this.chrono.text = this.ellapsedTime.ToString();
-    }
-
-    void Start() {
-        StopChrono();
-        ResetChrono();
-        StartChrono();
-    }
-
     void Update() {
-        if(!this.stopped) {
-            this.ellapsedTime = (float)(Math.Truncate((double)Time.time * 100.0) / 100.0);
-            this.ellapsedTime = (float)Math.Round(this.ellapsedTime, 2);
-            this.chrono.text = this.ellapsedTime.ToString();
-        }
+        this.time = (float)(Math.Truncate((double)Time.timeSinceLevelLoad * 100.0) / 100.0);
+        this.time = (float)Math.Round(this.time, 2);
+        this.chrono.text = this.time.ToString();
     }
 }
