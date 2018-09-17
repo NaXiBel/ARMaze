86fdc1fa10579348c48ba9959c376e3f2ea73ec6
Amélie <amelielupo@gmail.com>
2018-09-17 14:55:33 +0200

    Génération du sol du labyrinthe, boules et murs texturés

---
+++ gui/Assets/Resources/Scripts/Ball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+
+	private void InitializeBall() {
+
+	}
+	void Start () {
+		this.InitializeBall(); 
+	}
+	
+	private void Move() {
+
+	}
+	
+	void Update () {
+		
+	}
+}
