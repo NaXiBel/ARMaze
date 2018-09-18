19721846b8192b6451616f7ae4f3c5959c7481b0
Amélie <amelielupo@gmail.com>
2018-09-18 14:44:10 +0200

    Amélioration physique balle

--- gui/Assets/Wall.cs
+++ gui/Assets/Wall.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class Wall : MonoBehaviour {
 
+	private Ball ball = null;
+	private void RotateMazeTest() {
+		Vector3 eulerRotation = this.transform.eulerAngles;
+
+		if(Input.GetKey("left")) {
+			this.ball.WakeUp();
+			this.transform.rotation = Quaternion.Euler(eulerRotation.x, eulerRotation.y, eulerRotation.z + 5.0f);
+		}
+
+		if(Input.GetKey("right")) {
+			this.ball.WakeUp();
+			this.transform.rotation = Quaternion.Euler(eulerRotation.x, eulerRotation.y, eulerRotation.z - 5.0f);
+		}
+
+		if(Input.GetKey("up")) {
+			this.ball.WakeUp();
+			this.transform.rotation = Quaternion.Euler(eulerRotation.x + 5.0f, eulerRotation.y, eulerRotation.z);
+		}
+
+		if(Input.GetKey("down")) {
+			this.ball.WakeUp();
+			this.transform.rotation = Quaternion.Euler(eulerRotation.x - 5.0f, eulerRotation.y, eulerRotation.z);
+		}
+	}
 	// Use this for initialization
 	void Start () {
-		
+		this.ball = GameObject.Find(Const.BALL_GAMEOBJECT_NAME).GetComponent<Ball>(); 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		this.RotateMazeTest();
 	}
 }
