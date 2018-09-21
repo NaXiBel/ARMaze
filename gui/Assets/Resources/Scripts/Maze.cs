045d64964ada9b6f60ea4843528135afe9752b76
Amélie <amelielupo@gmail.com>
2018-09-21 11:45:12 +0200

    Implémentation réaliste de la physique de la balle

--- gui/Assets/Resources/Scripts/Maze.cs
+++ gui/Assets/Resources/Scripts/Maze.cs
@@ -10,22 +10,22 @@ public class Maze : MonoBehaviour {
 
 		if(Input.GetKey("left")) {
 			this.ball.WakeUp();
-			this.transform.rotation = Quaternion.Euler(eulerRotation.x, eulerRotation.y, eulerRotation.z + 5.0f);
+			this.transform.Rotate(eulerRotation.x, eulerRotation.y, eulerRotation.z + 5.0f);
 		}
 
 		if(Input.GetKey("right")) {
 			this.ball.WakeUp();
-			this.transform.rotation = Quaternion.Euler(eulerRotation.x, eulerRotation.y, eulerRotation.z - 5.0f);
+			this.transform.Rotate(eulerRotation.x, eulerRotation.y, eulerRotation.z - 5.0f);
 		}
 
 		if(Input.GetKey("up")) {
 			this.ball.WakeUp();
-			this.transform.rotation = Quaternion.Euler(eulerRotation.x + 5.0f, eulerRotation.y, eulerRotation.z);
+			this.transform.Rotate(eulerRotation.x + 5.0f, eulerRotation.y, eulerRotation.z);
 		}
 
 		if(Input.GetKey("down")) {
 			this.ball.WakeUp();
-			this.transform.rotation = Quaternion.Euler(eulerRotation.x - 5.0f, eulerRotation.y, eulerRotation.z);
+			this.transform.Rotate(eulerRotation.x - 5.0f, eulerRotation.y, eulerRotation.z);
 		}
 	}
 	// Use this for initialization
--- gui/Assets/Resources/Scripts/MazeObstacle.cs
+++ gui/Assets/Resources/Scripts/MazeObstacle.cs
@@ -4,24 +4,31 @@ using UnityEngine;
 
 public class MazeObstacle : MonoBehaviour {
 
-	private void GetDimensions() {
+	private static Vector3 GetObstacleScale(Vector3 p1, Vector3 p2) {
 
+		float scaleX = Mathf.Abs(p1.x - p2.x);
+		float scaleY = Mathf.Abs(p1.y - p2.y);
+		float scaleZ = Mathf.Abs(p1.z - p2.z);
+		
+		return new Vector3(scaleX, scaleY, scaleZ);
 	}
 
-	private void GetPosition() {
+	public static GameObject Create(Vector3 p1, Vector3 p2) {
+		
+		
+		GameObject prefab = ((GameObject)Resources.Load("Prefabs/" + Const.OBSTACLE_PREFAB_NAME, typeof(GameObject)));
+		Renderer rend = prefab.GetComponent<Renderer>();
+		Vector3 obstacleScale = MazeObstacle.GetObstacleScale(p1, p2);
+		prefab.transform.localScale = obstacleScale;
+		
+		rend.sharedMaterial.SetTextureScale("_MainTex", new Vector2(obstacleScale.x, obstacleScale.y));
+
+		GameObject o = Instantiate(prefab, Vector3.zero, Quaternion.Euler(0.0f, 0.0f, 0.0f) );
+		o.transform.parent = GameObject.Find(Const.MAZE_GAMEOBJECT_NAME).transform;
+
+		return o; 
 
 	}
-
-	private void InitializeWall() {
-		this.GetDimensions();
-		this.GetPosition();
-	}
-
-
-	void Start () {
-		this.InitializeWall();
-	}
-	
 
 	void Update () {
 		
