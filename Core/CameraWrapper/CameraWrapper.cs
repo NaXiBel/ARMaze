8589ae9bc992f65ae70a1e76f438837db3f31d22
alexmoutouh <alexmoutouh.global@gmail.com>
2018-10-22 10:20:48 +0200

    Rotation 180 + Unlit/Texture pour Quad de rendu.

--- Core/CameraWrapper/CameraWrapper.cs
+++ Core/CameraWrapper/CameraWrapper.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 namespace Wrapper {
     public class CameraWrapper {
         public struct embededFunctions {
-            //const string dllpath = "./VideoStream.dll";
-            const string dllpath = @"D:\ARMaze - Copie\Core\x64\Debug\VideoStream.dll";
+            const string dllpath = @".\VideoStream.dll";
+            //const string dllpath = @"D:\ARMaze - Copie\Core\x64\Debug\VideoStream.dll";
             [DllImport(dllpath, EntryPoint = "createCamera")]static public extern IntPtr CreateCameraInput();
             [DllImport(dllpath, EntryPoint = "disposeCamera")]static public extern void DisposeCameraInput(IntPtr pCam);
             [DllImport(dllpath, EntryPoint = "openStream")]static public extern void OpenStream(IntPtr pCam, int id = 0);
